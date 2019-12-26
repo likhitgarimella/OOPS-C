909213cd916fd66c6f0b9be00279abf27ecffdc9
Likhit Garimella <likhitgmella@gmail.com>
2019-12-26 10:22:19 +0530

    Add files via upload

---
+++ SumOf2Arrays.c
@@ -0,0 +1,42 @@
+/*
+ C program to find the sum of two one-dimensional arrays using
+ Dynamic Memory Allocation
+	*/
+#include <stdio.h>
+#include <malloc.h>
+
+void main()
+{
+    int i, n;
+	int *a, *b, *c;
+	
+    printf("How many Elements in each array \n");
+    scanf("%d", &n);
+ 
+    a = (int *)malloc(n * sizeof(int));
+    b = (int *)malloc(n * sizeof(int));
+	c = (int *)malloc(n * sizeof(int));
+	
+    printf("Enter Elements of First List\n");
+ 
+    for (i = 0; i < n; i++)
+        {
+  		scanf("%d", a + i);
+		}
+    printf("Enter Elements of Second List\n");
+    for (i = 0; i < n; i++)
+        {
+ 		scanf("%d", b + i);
+		}
+    for (i = 0; i < n; i++)
+        {
+		*(c + i) = *(a + i) + *(b + i);
+		}
+    printf("Resultant List is\n");
+    for (i = 0; i < n; i++)
+        {
+		printf("%d\n", *(c + i));
+		}
+		
+}
+
---
+++ SumOfArrayElem.c
@@ -0,0 +1,31 @@
+/*
+    C program to read N integers and store them in an array A.
+    Find the sum of all these elements using pointer.
+   		*/
+#include <stdio.h>
+#include <malloc.h>
+ 
+void main()
+{
+	int i, n, sum = 0;
+	int *a;
+ 
+    printf("Enter the size of array A \n");
+	scanf("%d", &n);
+ 
+    a = (int *) malloc(n * sizeof(int));
+ 
+    printf("Enter Elements of First List \n");
+	for (i = 0; i < n; i++) 
+        {
+		scanf("%d", a + i);
+		}
+ 
+        //Compute the sum of all elements in the given array
+        for (i = 0; i < n; i++)
+        {
+		sum = sum + *(a + i);
+		}
+        printf("Sum of all elements in array = %d\n", sum);
+ 
+}
